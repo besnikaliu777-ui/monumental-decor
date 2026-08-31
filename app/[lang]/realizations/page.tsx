29c17216dba90a1bcb52f0b177ff8e55735fcea4
besnikaliu777-ui <besnikaliu777@gmail.com>
2026-08-31 21:02:00 +0200

    Add files via upload

--- app/[lang]/realizations/page.tsx
+++ app/[lang]/realizations/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from 'next';
 import { getDictionary, Locale } from '../../../lib/translations';
 import Gallery from '../../../components/Gallery';
+import { pageMetadata } from '../../../lib/seo';
 
 interface Props {
   params: { lang: Locale };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return pageMetadata('realizations', params.lang, 'realizations');
 }
 
 export default async function RealizationsPage({ params }: Props) {
