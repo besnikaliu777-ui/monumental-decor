692273f0452ca6a1262aea2c2227191c770d0b9b
besnikaliu777-ui <besnikaliu777@gmail.com>
2026-09-01 21:56:08 +0200

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-// Redirect root URL to the default locale (French). Next.js will also handle
-// locale detection automatically, but we explicitly redirect here for clarity.
-export default function RootPage() {
-  redirect('/fr');
-}
